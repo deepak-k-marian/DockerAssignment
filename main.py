d9d966f79f0eee45f6ccfab73fce64f18045ea00
k-marian-deepak <kmariandeepak@gmail.com>
2026-09-09 16:21:59 +0530

    Updated to HTML/CSS/JS static/templates and integrated it with fastapi

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def helloWorld():
-	return {"message": "Hello World"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def response(request: Request):
+	return templates.TemplateResponse(
+		request=request,
+		name="index.html"
+	)
---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('main-btn');
+    
+    btn.addEventListener('click', () => {
+        alert("You clicked the Hello World button! The frontend is completely wired up.");
+    });
+});
